68256d59c376a183f2b38ed36350b2a4da7f91bd
formic043 <82488121+formic043@users.noreply.github.com>
2021-04-13 23:38:54 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+apiKey: "AIzaSyD--5dfvXwdt3kpsvUPAfnHSrvtHsDlEL4",
+    authDomain: "autentificy.firebaseapp.com",
+    projectId: "autentificy",
+    storageBucket: "autentificy.appspot.com",
+    messagingSenderId: "801955330380",
+    appId: "1:801955330380:web:118372f41a28684b6af7e9",
+    measurementId: "G-3R4ZK1FT61"
+});
